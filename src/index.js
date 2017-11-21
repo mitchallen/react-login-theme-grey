61ff0b22ae267bb3dc274b32b71792cab3caa1a0
Mitch Allen <mitch.c.allen@gmail.com>
2017-11-20 17:56:22 -0700

    init commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+/*
+    Module: @mitchallen/react-login-theme-grey
+    Author: Mitch Allen
+*/
+
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+class LoginThemeGrey extends React.Component {
+  render() {
+    return (
+    	<div>
+      		<div>Package: @mitchallen/react-login-theme-grey</div>
+      		<div>Component: LoginThemeGrey</div>
+      	</div>
+    );
+  }
+}
+
+// LoginThemeGrey.propTypes = {
+//   // someProp: PropTypes.isRequired,
+// };
+
+export default LoginThemeGrey;
