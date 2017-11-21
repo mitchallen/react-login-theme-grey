379af6200fb47d77483c103d6037f38e8acc47c3
Mitch Allen <mitch.c.allen@gmail.com>
2017-11-20 19:08:00 -0700

    updated smoke test and circleci react version

--- test/Smoke.spec.js
+++ test/Smoke.spec.js
@@ -6,16 +6,39 @@ const expect = Chai.expect;
 
 import LoginThemeGrey from '../src/index';
 
+function validateForm() {
+  console.log('validateForm()');
+  return true;
+}
+
+function handleChange(event) {
+  console.log( event.target.id, event.target.value );
+}
+
+async function handleSubmit (event) {
+  event.preventDefault();
+  console.log('handleSubmit()');
+}
+
 describe('LoginThemeGrey', () => {
 
   const renderer = new ShallowRenderer();
-  renderer.render(<LoginThemeGrey />);
+
+  renderer.render(<LoginThemeGrey 
+    handleChange={handleChange}
+    handleSubmit={handleSubmit}
+    validateForm={validateForm}
+    status='testing'
+    isLoading={true}
+    username='John'
+    password='Smith'
+  />);
+
   const result = renderer.getRenderOutput();
 
-
   it('root element should be a div', () => {
-    // expect(wrapper.type()).to.eql('div');
-    expect(result.type).to.eql('div');
+    // console.log(JSON.stringify(result));
+    expect(result.props.children.type).to.eql('div');
   });
 
 });
